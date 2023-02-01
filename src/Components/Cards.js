61ac8c8a224402ccb949636ff7abc1b87900b967
Akash <shindeakashak47@gmail.com>
2023-02-01 21:47:43 +0530

    Added chat button visibility on click

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,72 +1,100 @@
-import { ethers } from 'ethers';
-import React, { useState } from 'react'
-import { contractABI, contractAddress } from '../Constants/Constants';
-
+import { ethers } from "ethers";
+import React, { useState } from "react";
+import { contractABI, contractAddress } from "../Constants/Constants";
 import * as PushAPI from "@pushprotocol/restapi";
 import { Chat } from "@pushprotocol/uiweb";
 
 
 
-function Cards({Name, Desc, Date, Img, projectId, projectVotes}) {
-
+function Cards({ Name, Desc, Date, Img, projectId, projectVotes }) {
+  const [chatBtn, setChatBtn] = useState(false);
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   const signer = provider.getSigner();
 
+  const voteProjectFun = async () => {
+    const contractInstance = new ethers.Contract(
+      contractAddress,
+      contractABI,
+      signer
+    );
+    // console.log("projectId", projectId)
 
-    const voteProjectFun = async() => {
-      const contractInstance = new ethers.Contract(contractAddress, contractABI, signer);
-      // console.log("projectId", projectId)
-
-      try {
-        
-        const voteProjectTx = await contractInstance.voteProject(projectId)
-        await voteProjectTx.wait();
-        window.alert("Successfully Voted")
-      } catch (error) {
-          window.alert(error)
-      }
+    try {
+      const voteProjectTx = await contractInstance.voteProject(projectId);
+      await voteProjectTx.wait();
+      window.alert("Successfully Voted");
+    } catch (error) {
+      window.alert(error);
     }
+  };
 
-    const chatWithOwner = async() => {
+  // const chatWithOwner = async() => {
 
-      return (
-        <>
-            <h2>Push Chat test</h2>
-    
-    
-                        <Chat
-                        // account={account} //user address 
-                        account= "0x88Ab2b62ccBD5170AA4D7266C0D5d7D002689fEf" //user address 
-                        supportAddress="0xc221979949e0ACc4E1E715FbB232284f7eE412d4" //support address
-                        // apiKey="jVPMCRom1B.iDRMswdehJG7NpHDiECIHwYMMv6k2KzkPJscFIDyW8TtSnk4blYnGa8DIkfuacU0"
-                        apiKey="vzOQa8Hda3.lD6Yvrij1T4qHrE07Mp7XcE3mRWu8Yl6WAmOzLSfI63xWuGSoNkXsHeBDVvG63Hs"
-                            env="staging"
-            />
-    
-            
-            
-        </>
-      )
+  //   return (
+  //     <>
+  //         <h2>Push Chat test</h2>
+  //                     <Chat
+  //                     // account={account} //user address
+  //                     account= "0x88Ab2b62ccBD5170AA4D7266C0D5d7D002689fEf" //user address
+  //                     supportAddress="0xc221979949e0ACc4E1E715FbB232284f7eE412d4" //support address
+  //                     // apiKey="jVPMCRom1B.iDRMswdehJG7NpHDiECIHwYMMv6k2KzkPJscFIDyW8TtSnk4blYnGa8DIkfuacU0"
+  //                     apiKey="vzOQa8Hda3.lD6Yvrij1T4qHrE07Mp7XcE3mRWu8Yl6WAmOzLSfI63xWuGSoNkXsHeBDVvG63Hs"
+  //                         env="staging"
+  //         />
 
-    }
+  //     </>
+  //   )
 
-
-  return (
+  // }
+ 
+  return chatBtn?((
     <>
-        <div className="card" style={{width: "18rem"}}>
-          <img className="card-img-top" src={Img} alt="Card image cap"/>
-          <div className="card-body">
-            <h5 className="card-title">{Name}</h5>
-            <p className="card-text">{Desc}</p>
-            <p>{Date}</p>
-            <p>{`Votes:- ${projectVotes}`}</p>
-            <button onClick={voteProjectFun} className="btn btn-primary">Vote Project</button>
-            <button onClick={chatWithOwner} className="btn btn-primary">Chat</button>
-          </div>
+      <div className="card" style={{ width: "18rem" }}>
+        <img className="card-img-top" src={Img} alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">{Name}</h5>
+          <p className="card-text">{Desc}</p>
+          <p>{Date}</p>
+          <p>{`Votes:- ${projectVotes}`}</p>
+          <button onClick={voteProjectFun} className="btn btn-primary">
+            Vote Project
+          </button>
+          <button  className="btn btn-primary">
+            Chat
+          </button>
         </div>
+      </div>
+
     
+      <Chat
+        // account={account} //user address
+        account="0x88Ab2b62ccBD5170AA4D7266C0D5d7D002689fEf" //user address
+        supportAddress="0xc221979949e0ACc4E1E715FbB232284f7eE412d4" //support address
+        // apiKey="jVPMCRom1B.iDRMswdehJG7NpHDiECIHwYMMv6k2KzkPJscFIDyW8TtSnk4blYnGa8DIkfuacU0"
+        apiKey="vzOQa8Hda3.lD6Yvrij1T4qHrE07Mp7XcE3mRWu8Yl6WAmOzLSfI63xWuGSoNkXsHeBDVvG63Hs"
+        env="staging"
+      />
+   
     </>
-  )
+  )):(<>
+           <div className="card" style={{ width: "18rem" }}>
+        <img className="card-img-top" src={Img} alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">{Name}</h5>
+          <p className="card-text">{Desc}</p>
+          <p>{Date}</p>
+          <p>{`Votes:- ${projectVotes}`}</p>
+          <button onClick={voteProjectFun} className="btn btn-primary">
+            Vote Project
+          </button>
+          <button onClick={()=>setChatBtn(true)} className="btn btn-primary">
+            Chat
+          </button>
+        </div>
+      </div>
+ 
+  </>);
+ 
 }
 
-export default Cards
+export default Cards;
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,11 +1,13 @@
-import { ethers } from 'ethers';
-import React, { useState } from 'react'
-import { contractABI, contractAddress } from '../Constants/Constants';
-import Cards from './Cards';
+import { ethers } from "ethers";
+import React, { useState } from "react";
+import { contractABI, contractAddress } from "../Constants/Constants";
+import Cards from "./Cards";
+import '../CSS/Products.css'
 
 function Products() {
-  
-  const [imgTest, setImgTest] = useState("https://bafybeifwmvex2sqtjej66g7nsgaoiqjeixeaklkahfqeun354dnhbibgea/testProject.jpeg")
+  const [imgTest, setImgTest] = useState(
+    "https://bafybeifwmvex2sqtjej66g7nsgaoiqjeixeaklkahfqeun354dnhbibgea/testProject.jpeg"
+  );
 
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   const signer = provider.getSigner();
@@ -14,14 +16,16 @@ function Products() {
   // Will containg all the available products, fetched from SC
   let dataArray = [];
 
-  const getListedProjects = async() => {
-
-
-    const contractInstance = new ethers.Contract(contractAddress, contractABI, provider);
+  const getListedProjects = async () => {
+    const contractInstance = new ethers.Contract(
+      contractAddress,
+      contractABI,
+      provider
+    );
     const totalProjects = await contractInstance.listedProjects();
 
-    for(let i = 1; i <= totalProjects.toString(); i ++){
-      let projects = await contractInstance.currentProjectMapping(i)
+    for (let i = 1; i <= totalProjects.toString(); i++) {
+      let projects = await contractInstance.currentProjectMapping(i);
 
       // console.log(projects[0].toString());// Project Id
       // console.log(projects[1].toString());// Project Details
@@ -30,52 +34,57 @@ function Products() {
       // console.log(projects[4].toString());// Shark Vote
       // console.log(projects[5].toString());// User Address
 
-      
-      let ipfsLink = (projects[1].toString()).split("/")  ;// Project Details
-      let ipfsCID = ipfsLink[2]
+      let ipfsLink = projects[1].toString().split("/"); // Project Details
+      let ipfsCID = ipfsLink[2];
 
-      const projectDesc = await fetch(`https://${ipfsCID}.ipfs.dweb.link/metadata.json`).then(response => response.json());
+      const projectDesc = await fetch(
+        `https://${ipfsCID}.ipfs.dweb.link/metadata.json`
+      ).then((response) => response.json());
 
       // Returns data from the link.
-      console.log("Name:- ", projectDesc["name"])
-      console.log("Description:- ", projectDesc["description"])
-      console.log("ProjectEndDate:- ", projectDesc["ProjectEndDate"])
-      console.log("Image Url:- ", projectDesc["image"])
-      
-      var date = new Date(projectDesc["ProjectEndDate"] * 1000);
-      console.log("date:- ", date.toLocaleDateString("default"))
-      let testImgURL = (projectDesc["image"].toString()).split("//");
-                                                                                                                                               // Project ID               Project Votes
-      dataArray.push([projectDesc["name"], projectDesc["description"], date.toLocaleDateString("default"),`https://ipfs.io/ipfs/${testImgURL[1]}`,projects[0].toString() , projects[3].toString()])
+      console.log("Name:- ", projectDesc["name"]);
+      console.log("Description:- ", projectDesc["description"]);
+      console.log("ProjectEndDate:- ", projectDesc["ProjectEndDate"]);
+      console.log("Image Url:- ", projectDesc["image"]);
 
+      var date = new Date(projectDesc["ProjectEndDate"] * 1000);
+      console.log("date:- ", date.toLocaleDateString("default"));
+      let testImgURL = projectDesc["image"].toString().split("//");
+      // Project ID               Project Votes
+      dataArray.push([
+        projectDesc["name"],
+        projectDesc["description"],
+        date.toLocaleDateString("default"),
+        `https://ipfs.io/ipfs/${testImgURL[1]}`,
+        projects[0].toString(),
+        projects[3].toString(),
+      ]);
     }
 
     setDetails(dataArray);
-    console.log("dataArray:- ", dataArray)
-    console.log("setDetails", details)
-    
-  }
+    console.log("dataArray:- ", dataArray);
+    console.log("setDetails", details);
+  };
 
   return (
     <div>
-        <h1>Listed projects</h1>
-
-        <br /><br />
-        
-        {details.map(details => (
-
-
-        <Cards Name = {details[0]} Desc = {details[1]} Date = {details[2]} Img = {details[3]} projectId = {details[4]} projectVotes = {details[5]} />
+      <h1>Listed projects</h1>
+      <div className="alignCards">
+      {details.map((details) => (
+        <Cards
+          Name={details[0]}
+          Desc={details[1]}
+          Date={details[2]}
+          Img={details[3]}
+          projectId={details[4]}
+          projectVotes={details[5]}
+        />
         // <Cards Name = {details[0]} Desc = {details[1]} Date = {details[2]} Img = {"https://bafybeihngwk3dggkoqmxcjlpoqabmli6sofx6f2iv6hnd5y4s4hzn4xtje.ipfs.dweb.link/The-Web3-Project-Makes-A-Move-Into-The-Metaverse.jpg"} />
-        ))}
-        <hr/>
-        
-        {/* <br /><br /> */}
-        <hr />
-
-        <button onClick={getListedProjects}>Get Listed Projects</button>
+      ))}
+ </div>
+      <button onClick={getListedProjects}>Get Listed Projects</button>
     </div>
-  )
+  );
 }
 
-export default Products
+export default Products;
